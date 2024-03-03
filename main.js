30b8166d7394c1696f8ed98f8c8c489b56fcb7c7
umaimaa09 <161639086+umaimaa09@users.noreply.github.com>
2024-03-03 23:07:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// Example Of String Data Type With Concatenation
+var anycountry = ("Pakistan");
+console.log("I Live in" + anycountry + ".");
+// Example Of Number Data Type With Arithmetic Operation
+var m = 5;
+var n = 9;
+console.log(m + n);
+console.log(m - n);
+console.log(m * n);
+console.log(m / n);
+console.log(m % n);
+console.log(Math.pow(m, 4));
+console.log(m * n / 7);
+// Example Of Boolean Data Types
+var t = true;
+var f = false;
+console.log("students have done their homework", t);
+console.log("students has done their homework", f);
+// Example Of Null Data Types
+var number = null;
+console.log(number);
+// Example Of Undefined Data Types
+var city;
+console.log(city);
+// Example Of Symbol Data Type
+var x = Symbol('fullstop');
+console.log(x);
+//Example Of BigInt Data Type
+var value = BigInt(904000);
+console.log(value);
---
+++ main.ts
@@ -0,0 +1,30 @@
+// Example Of String Data Type With Concatenation
+let anyCountry = ("Pakistan");
+console.log(" I Live in " + anyCountry + ".");
+// Example Of Number Data Type With Arithmetic Operation
+var m = 5;
+var n = 9;
+console.log(m + n);
+console.log(m - n);
+console.log(m * n);
+console.log(m / n);
+console.log(m % n);
+console.log(m**4);
+console.log(m*n/7);
+// Example Of Boolean Data Types
+const correct:Boolean=true;
+const incorrect:Boolean= false;
+console.log("students have done their homework", correct);
+console.log("students has done their homework", incorrect);
+// Example Of Null Data Types
+const num:null = null;
+console.log(num);
+// Example Of Undefined Data Types
+let area: undefined;
+console.log(area);
+// Example Of Symbol Data Type
+let y = Symbol ('fullstop');
+ console.log(y);
+ //Example Of BigInt Data Type
+ let bvalue = BigInt(904000);
+ console.log(bvalue);
